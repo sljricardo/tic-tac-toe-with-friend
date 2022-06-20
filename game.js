721ad473e57d603376f177e3b200cff971be86fd
pfconceicao <106524075+pfconceicao@users.noreply.github.com>
2022-06-20 11:03:24 +0100

    comment rule 2

--- game.js
+++ game.js
@@ -10,4 +10,12 @@
 
 // REGRA 1 //
 
-/* Será criado um array multidimensional comsposto por 3 arrays. Cada array equivalerá a uma linha da grelha */
+/* Será criado um array multidimensional composto por 3 arrays. 
+   Cada array equivalerá a uma linha da grelha */
+
+// REGRA 2 //
+
+/* Cada jogada é intercalada (começa o jogador 1 e de seguida joga o jogador 2).
+   Sempre que um jogador clicar sobre um quadrado em branco, esse quadrado deverá ficar marcado com 
+   um símbolo (X ou O). Se esse espaço já estiver preenchido, o programa não deverá fazer nada.
+   O primeiro click é o jogador 1 (impar) e o segundo click é o jogador 2 (par). */
