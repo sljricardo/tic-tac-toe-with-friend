d61e2cde7b0f3221c469a7f54714edda1673557e
pfconceicao <106524075+pfconceicao@users.noreply.github.com>
2022-06-20 10:52:52 +0100

    comment rule 1

--- game.js
+++ game.js
@@ -2,8 +2,12 @@
 
 /* REGRAS
 
-Tabuleiro com 9 espaços em uma grelha 3X3
+1 - Tabuleiro com 9 espaços em uma grelha 3X3
 
-2 jogadores um X e outro O
+2 - 2 jogadores um X e outro O
 
-o primeiro a completar 3 em linha ganha */
+3 - o primeiro a completar 3 em linha ganha */
+
+// REGRA 1 //
+
+/* Será criado um array multidimensional comsposto por 3 arrays. Cada array equivalerá a uma linha da grelha */
